e8b4802712d4d15dd42b1c3dcc6745720e8a7929
Mike Pesavento <mike@peztek.com>
2024-06-16 02:29:50 -0700

    add langgraph agent notebook start

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ai_agent_examples',
+    packages=find_packages(),
+    version='0.1.0',
+    description='examples of agentic AI',
+    author='Mike Pesavento',
+    license='©2024 PezTek Consulting LLC',
+)
